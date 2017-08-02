1d070bf23a2b31662547cfbe235359cf5b759d96
Shwb <shwb95@163.com>
2017-08-02 22:33:37 +0800

    Update loading.py

--- loading.py
+++ loading.py
@@ -1,15 +1,39 @@
 # -*- coding: utf-8 -*-
-# 电脑“想了想”,然后发出了疑惑“嗯哼？”
+"""
+python2实现命令行动态载入的显示效果
+https://www.zhihu.com/question/21100416
+"""
 
-from time import sleep
 import sys
+import time
+import random
+from string import ascii_letters
 
-itime = 0
+print 'Loading....  ',
+sys.stdout.flush()
 
-while itime < 3:
-    for i in r'\|/-|/':
-        sys.stdout.write(i + '\r')
-        sleep(0.08)
-        itime += 0.08
+i = 0
+spins = ['/', '-', '\\', '|']
+
+while i <= 10:
+    sys.stdout.write('\b{}'.format(spins[i%4]))
+    sys.stdout.flush()
+    time.sleep(0.2)
+    i += 1
+
+print
+print 'Loading....  ',
+sys.stdout.flush()
+i = 0
+length = 1
+
+while i <= 10:
+    l = random.randint(1, 10)
+    num = ''.join(random.sample(ascii_letters, l))
+    sys.stdout.write('{}{}\033[K'.format('\b' * length, num))
+    sys.stdout.flush()
+    time.sleep(0.2)
+    length = l
+    i += 1
 
 print u"嗯哼？"
