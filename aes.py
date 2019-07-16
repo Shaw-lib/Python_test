6301074915c5d4d7289fd8c750c374059597c912
Shao <shwb95@163.com>
2019-07-16 18:31:10 +0800

    Add files via upload

---
+++ aes.py
@@ -0,0 +1,38 @@
+# pycryptodome==3.8.2
+
+import base64
+
+from Crypto.Cipher import AES
+
+class AESCipher(object):
+    def __init__(self, key):
+        self.bs = 16
+        self.cipher = AES.new(key, AES.MODE_ECB)
+
+    def encrypt(self, raw):
+        raw = self._pad(raw)
+        encrypted = self.cipher.encrypt(raw.encode('utf-8'))
+        encoded = base64.b64encode(encrypted)
+        return str(encoded, 'utf-8')
+
+    def decrypt(self, raw):
+        decoded = base64.b64decode(raw)
+        decrypted = self.cipher.decrypt(decoded)
+        return str(self._unpad(decrypted), 'utf-8')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+
+    def _unpad(self, s):
+        return s[:-ord(s[len(s)-1:])]
+
+if __name__ == '__main__':
+    key = b'abcefposjksdklio' # must 16
+    cipher = AESCipher(key)
+
+    plaintext = '1'
+    encrypted = cipher.encrypt(plaintext)
+    print(f'Encrypted: {encrypted}')
+
+    decrypted = cipher.decrypt(encrypted)
+    print(f'Decrypted: {decrypted}')
